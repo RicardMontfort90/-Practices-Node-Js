aa2e6bcde82a1677a68c4aced1bb8264df7e0851
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-02 14:15:53 +0100

    work in progress

--- 4.fs-async-await.js
+++ 4.fs-async-await.js
@@ -4,11 +4,13 @@
 //const { promisify } = require('node:util')
 //const readFilePromise = promisify(fs.readFile)
 
-const { readFile } = require('node:fs/promises')
+const { readFile } = require('node:fs/promises');
 
 
-
-console.log('Leyendo el primer archivo... ')
+// IIFE - Inmediatly Invoked Function Expression (FUNCIÓN AUTOINVOCADA)
+(
+    async () => {
+    console.log('Leyendo el primer archivo... ')
 const firstText = await readFile('./archivo.txt', 'utf-8')
 console.log('primer texto:', firstText)
 console.log('Hacer cosas mientras lee el archivo...')
@@ -16,4 +18,8 @@ console.log('Hacer cosas mientras lee el archivo...')
 console.log('Leyendo el segundo archivo... ')
 const secondText = await readFile('./archivo2.txt', 'utf-8')
 console.log('segundo texto', secondText)
+    }
+)()
+
+
 
