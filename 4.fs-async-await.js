f4e1a5878fbdaee8faee81352c3ee7596091569f
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-02 15:14:53 +0100

    work in progress

--- 4.fs-async-await.js
+++ 4.fs-async-await.js
@@ -11,8 +11,8 @@ const { readFile } = require('node:fs/promises');
 (
     async () => {
     console.log('Leyendo el primer archivo... ')
-const firstText = await readFile('./archivo.txt', 'utf-8')
-console.log('primer texto:', firstText)
+const text = await readFile('./archivo.txt', 'utf-8')
+console.log('primer texto:', text)
 console.log('Hacer cosas mientras lee el archivo...')
 
 console.log('Leyendo el segundo archivo... ')
--- 4.fs-async-await.mjs
+++ 4.fs-async-await.mjs
@@ -7,8 +7,8 @@
 import { readFile } from 'node:fs/promises'
 
 console.log('Leyendo el primer archivo... ')
-const firstText = await readFile('./archivo.txt', 'utf-8')
-console.log('primer texto:', firstText)
+const text = await readFile('./archivo.txt', 'utf-8')
+console.log('primer texto:', text)
 console.log('Hacer cosas mientras lee el archivo...')
 
 console.log('Leyendo el segundo archivo... ')
